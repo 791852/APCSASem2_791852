c228dbd89a41cdda8838391f09b78f574a6058e6
791852 <42598860+791852@users.noreply.github.com>
2019-01-10 10:01:08 -0800

    Lab 1/10 is now done too, so there is a bubleSort and a selectionSort in here with mySort

--- Lab 109/Lab 109/LabClass.java
+++ Lab 109/Lab 109/LabClass.java
@@ -1,4 +1,3 @@
-import java.util.*;
 /**
  * Veronica Pratt
  * 
@@ -11,10 +10,12 @@ public class LabClass
             int random = (int)(Math.random()*11);
             nums[i] =random;
         }
-        sortList(nums); //sorts array 
-        listPrint(nums); //prints out sorted array check
+        mySort(nums); //sorts array in each one
+        bubbleSort(nums);
+        selectionSort(nums);
+        listPrint(nums); //prints out sorted array as a check
     }
-    public static int[] sortList(int list[]){
+    public static int[] mySort(int list[]){
         for(int i = list.length; i>1; i--){ //allows for loops to check every value in array against every other value
             for(int j = 0; j<list.length-1; j++){ 
                 if(list[j]>list[j+1]){ //compares every value to the next
@@ -22,6 +23,32 @@ public class LabClass
                     list[j] = list[j+1];
                     list[j+1] = greater; //changes out values and sorts array
                 }
+            }
+        }
+        return list;
+    }
+    public static int[] bubbleSort(int list[]){
+        for(int i = list.length; i>1; i--){ //allows for loops to check every value in array against every other value
+            for(int j = 0; j<list.length-1; j++){ 
+                if(list[j]>list[j+1]){ //compares every value to the next
+                    int greater =  list[j];
+                    list[j] = list[j+1];
+                    list[j+1] = greater; //changes out values and sorts array
+                }
+            }
+        }
+        return list;
+    }
+    public static int[] selectionSort(int list[]){
+        for(int i = list.length; i>0; i--){ //allows for loops to check every value in array against every other value
+            int index = i;
+            for(int j = i+1; j<list.length; j++){ 
+                if(list[j]<list[index]){ //compares to values of array and switches the indeces of the two as needed
+                    index = j;
+                }
+                int smallerNumber = list[index]; //finds the smaller value and sets it to a number
+                list[index] = list[i]; //sets the smaller number to the lower index
+                list[i] = smallerNumber;
             }
         }
         return list;
