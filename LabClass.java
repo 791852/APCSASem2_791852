b3e39457e7b70bd93b22fb0daea63decd1c6d6ac
791852 <42598860+791852@users.noreply.github.com>
2019-01-15 18:47:10 -0800

    Lab 109 Completed

---
+++ LabClass.java
@@ -0,0 +1,76 @@
+/**
+ * Veronica Pratt
+ * 
+ */
+public class LabClass
+{
+     public static void mainmySort(){
+        int nums [] = new int [20]; //creates array to work with
+        for(int i =0; i < nums.length; i++){ //fills array with random numbers
+            int random = (int)(Math.random()*11);
+            nums[i] =random;
+        }
+        mySort(nums); //sorts array in each one
+        listPrint(nums);}
+
+    public static void mainBubbleSort(){
+        int nums [] = new int [20]; //creates array to work with
+        for(int i =0; i < nums.length; i++){ //fills array with random numbers
+            int random = (int)(Math.random()*11);
+            nums[i] =random;
+        }
+        bubbleSort(nums); //sorts array in each one
+        listPrint(nums);}
+
+    public static void mainSelectionSort(){
+        int nums [] = new int [20]; //creates array to work with
+        for(int i =0; i < nums.length; i++){ //fills array with random numbers
+            int random = (int)(Math.random()*11);
+            nums[i] =random;
+        }
+        selectionSort(nums);
+        listPrint(nums);}
+    public static int[] mySort(int list[]){
+        for(int i = list.length; i>1; i--){ //allows for loops to check every value in array against every other value
+            for(int j = 0; j<list.length-1; j++){ 
+                if(list[j]>list[j+1]){ //compares every value to the next
+                    int greater =  list[j];
+                    list[j] = list[j+1];
+                    list[j+1] = greater; //changes out values and sorts array
+                }
+            }
+        }
+        return list;
+    }
+    public static int[] bubbleSort(int list[]){
+        for(int i = list.length; i>1; i--){ //allows for loops to check every value in array against every other value
+            for(int j = 0; j<list.length-1; j++){ 
+                if(list[j]>list[j+1]){ //compares every value to the next
+                    int greater =  list[j];
+                    list[j] = list[j+1];
+                    list[j+1] = greater; //changes out values and sorts array
+                }
+            }
+        }
+        return list;
+    }
+    public static int[] selectionSort(int list[]){
+        for(int i = 0; i<list.length; i--){ //allows for loops to check every value in array against every other value
+            int index = i;
+            for(int j = i+1; j<list.length; j++){ 
+                if(list[j]<list[index]){ //compares two values of array and switches the indeces of the two as needed
+                    index = j;
+                }
+                int smallerNumber = list[index]; //finds the smaller value and sets it to a number
+                list[index] = list[i]; //sets the smaller number to the lower index
+                list[i] = smallerNumber;
+            }
+        }
+        return list;
+    }
+    public static void listPrint(int list[]){
+        for(int i = 0; i < list.length; i++){ //goes through every value in the array and prints it
+            System.out.print(" " + list[i] + " ");
+        }
+    }
+} 
